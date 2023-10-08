b83ad979e7ddc8aa46e2f2620a95f9964640367a
Michael <auermich@fim.uni-passau.de>
2023-10-08 22:48:54 +0200

    fix verification issue: use appropriate const instruction upon conflicted register type

--- basicBlockCoverage/src/main/java/de/uni_passau/fim/auermich/instrumentation/basicblockcoverage/core/Instrumentation.java
+++ basicBlockCoverage/src/main/java/de/uni_passau/fim/auermich/instrumentation/basicblockcoverage/core/Instrumentation.java
@@ -346,7 +346,6 @@ public final class Instrumentation {
                 // object type
                 Opcode moveObject = Opcode.MOVE_OBJECT_FROM16;
 
-
                 LOGGER.debug("Object type!");
 
                 int destinationRegisterID = destinationRegisters.get(index);
@@ -393,8 +392,19 @@ public final class Instrumentation {
                      *             before it can be read from.
                      */
                     LOGGER.debug("Conflicted type: " + sourceRegisters.get(index));
-                    final BuilderInstruction11n setZero = new BuilderInstruction11n(Opcode.CONST_4, sourceRegisters.get(index), 0);
-                    mutableMethodImplementation.addInstruction(pos, setZero);
+
+                    if (sourceRegisters.get(index) < 16) {
+                        // CONST_4 is sufficient
+                        final BuilderInstruction11n setZero
+                                = new BuilderInstruction11n(Opcode.CONST_4, sourceRegisters.get(index), 0);
+                        mutableMethodImplementation.addInstruction(pos, setZero);
+                    } else {
+                        // CONST_4 can only handle v0-v15, thus use regular CONST instruction with support up to v255
+                        final BuilderInstruction31i setZero
+                                = new BuilderInstruction31i(Opcode.CONST, sourceRegisters.get(index), 0);
+                        mutableMethodImplementation.addInstruction(pos, setZero);
+                    }
+
                     pos++;
                 }
 
